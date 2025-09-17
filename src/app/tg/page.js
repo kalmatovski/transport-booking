7ba7abc1f55df9b7c7dda32914e026b50f44ecc9
Hussein <hussejnali3@gmail.com>
2025-09-17 21:29:28 +0600

    change tg

--- src/app/tg/page.js
+++ src/app/tg/page.js
@@ -1,15 +1,5 @@
-// app/tg/page.js
-'use client';
-
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import { redirect } from 'next/navigation';
 
 export default function TgEntry() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.replace('/'); // просто ведём на обычный корень
-  }, [router]);
-
-  return null;
+  redirect('/'); // сразу на главную сайта
 }
